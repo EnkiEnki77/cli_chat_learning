0519d84a5cb45c475decc1c9fe83d7bc9d9b7c00
Enki Layman <layman212@gmail.com>
2025-03-08 04:49:36 -0500

    Learned how to setup the Open AI api and build a function that inputs a prompt to a gpt model, and returns the response.

--- get_env_variable.py
+++ get_env_variable.py
@@ -1,5 +1,10 @@
-# We can utilize the os package to access our environment variables
+# We can utilize the os and dotenv packages to access our environment variables
 import os
+from dotenv import load_dotenv
 
-api_key = os.getenv('API_KEY')
+# By default load_dotenv assumes .env is in the same directory as the script, but you can configure
+# that with load_dotenv('/custom/path/.env')
+load_dotenv()
+
+api_key = os.environ.get("open_ai_api_key")
 print(api_key)
---
+++ open_ai.py
@@ -0,0 +1,28 @@
+# The Open AI API gives access to all of Open AI's various LLM's in your application
+import os
+from dotenv import  load_dotenv
+from openai import OpenAI
+from pyexpat.errors import messages
+
+load_dotenv()
+
+api_key = os.environ.get("open_ai_api_key")
+base_url = os.environ.get("base_url")
+
+client = OpenAI(api_key=api_key, base_url=base_url)
+
+
+def get_chat_completion(messages):
+    return client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0
+    )
+
+prompt = "Give a concise explanation of the difference between static arrays and dynamic arrays."
+messages = [{"role": "user", "content": prompt}]
+
+chat_completion = get_chat_completion(messages)
+gpt_response = chat_completion.choices[0].message.content
+
+print(gpt_response)
